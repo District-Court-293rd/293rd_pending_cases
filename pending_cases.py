42af801553e01cecb7ff8589f2512f1c7aab2844
Hector Rodriguez Jr <hector.rodriguez3@gmail.com>
2022-01-19 21:01:35 -0600

    remove unused impports

--- pending_cases.py
+++ pending_cases.py
@@ -1,11 +1,8 @@
 import streamlit as st
-import streamlit.components.v1 as stc
 import pandas as pd
-#import docx2txt
 from PyPDF2 import PdfFileReader
 import re
 import gspread
-import df2gspread as d2g
 
 def read_pdf(file):
     pdfReader = PdfFileReader(file) #reads pdf
