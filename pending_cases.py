28323b3ee85d8c6a4092a934055521b66b3abb59
Hector Rodriguez Jr <hector.rodriguez3@gmail.com>
2022-02-07 23:22:48 -0600

    mod

--- pending_cases.py
+++ pending_cases.py
@@ -79,8 +79,8 @@ civil_pending_notes_tab.update([ready_to_work_pending_list.columns.values.tolist
 
 
 #########Calculates counts
-total = ready_to_work_pending_list['cause_number'].count() #Counts total pending cases
-worked = ready_to_work_pending_list['notes'].notnull().sum()
+total = (ready_to_work_pending_list['cause_number']).count() #Counts total pending cases
+worked = (ready_to_work_pending_list['notes']).notnull().sum()
 disposed = (ready_to_work_pending_list['disposed']).value_counts()['TRUE']#Counts the total of disposed cases
 remaing_cases_to_be_worked = total - worked #Calculates the remaining cases to be disposed of
 
@@ -96,4 +96,4 @@ st.write('Worked:', worked)
 #total where disposed is TRUE
 st.write('Total Disposed:', disposed)
 #Total minus total disposed
-st.write ('Cases Remaining to be Worked', remaing_cases_to_be_worked )
+st.write ('Cases Remaining to be Worked', remaing_cases_to_be_worked)
