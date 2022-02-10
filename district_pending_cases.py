34c7fd1c3a6231e99aca298ba6b676dd17220083
Hector Rodriguez Jr <hector.rodriguez3@gmail.com>
2022-02-09 23:42:46 -0600

    Added title and uploader

--- district_pending_cases.py
+++ district_pending_cases.py
@@ -11,3 +11,12 @@ import openpyxl
 
 import jsmith_acquire
 import jsmith_prepare
+
+#title
+def main():
+    st.title("293rd District Court Civil and Criminal Pending Cases")
+if __name__ == '__main__':
+	main() 
+
+#uploader
+docx_file = st.file_uploader('Upload pdf', type = 'pdf')
