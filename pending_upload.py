f61239acce36f0ba5c6a562a828055f513a25d40
Johnathon Smith <smith.johnathon01@gmail.com>
2022-02-27 11:49:52 -0600

    Removed sensitive credential information and replaced using streamlit secrets

--- pending_upload.py
+++ pending_upload.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import numpy as np
 import pandas as pd
 import gspread
@@ -5,18 +6,17 @@ import gspread
 import jsmith_acquire
 import jsmith_prepare
 
-#JSON credentials for accessing the Pending Reports Spreadsheet
 credentials = {
-  "type": "service_account",
-  "project_id": "rd-spreadsheet-test",
-  "private_key_id": "1faeb4841e331afa6694c29e689ebfea2b909c93",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCDIp2Wh+zVQE0y\ncRVXAZLUstsBc8we+wLuMJm4PvkvAcMG6TjfkCmYYVfLRObKUkRjGCeXchfQIutl\nH54kJrL/WnkLUTuWmHcGNp9DIm5ukdO5wHmBW4YxeGpXffBL00aZybXZQjIj0xJy\n0Zb/KADN9XXNwAHazJsDA45jAJcyN0ZLnwIkYjtbROHPGcTDWkd1WbluwYu+joHp\n+Qib2lHoEGx9Evm56/QjpS2NuL0+dC64xs8kCmHSWNJYf1hrwxmmNF6dWL8pU6ET\nHmktoLc4fCR2lXQbRUbJVx63QqK0fKR97nOrXKLU42JmH4O0Sa3FCRF8fos8yaIO\n/MgsVsptAgMBAAECggEADbiuB6XCZM/9I+WMR5XdEM+ENgMxXR6DwHuqYgqI1V/Y\nUd1Oe6ogiBbLcnQLuCjiigHphqHEFx3IRsUHrZ21Bh4n40TFOO1MKwTFJmewaXZt\n0pZuwFfc00lXyz7cyXpafVYLZqfUCkaYPBZtN9auOt7PzE8mgof9QlP5bqeNqo3S\nv6Hjmzkn3ht9rq8i6QWAXqaJVsnOurVnDGXdTdD4Tk5vK0VYmu2DOkM60xxTx0tC\nM2Lm0tI46czQydZopf+yH9gP0LAUkATZLLwxP0PcnmS/I9hj6vdJRq64jWHYUwvx\npezLdSh5pYstBSqiKIbkn3RlXnT7dFL7PWxVJmNL6wKBgQC3s4BDna/yHVwkMGpK\nbbb8tO6gNEBNvWLHonVqQDd/+p4Ar2GxTsdckLfwMdjqgAleTWndoZ9QScmbZdpS\nvdND+2Y4RkX2CMz+UzmhKVVE5dwY0tO8g8Tqy+/+5l3s9uaqST495QCUqI1lDGlI\nBJ4CWOVnoWbbxq//Xl8samclXwKBgQC2vuvCYsFTxzNa9xMEvBjfC37KjyX3X3vQ\nA3/lhgP9M/T9Eihf6I2nzYUZu2ZQTazQ9I1V8PRrq1M4RHLxUZDvA4QwukRL4GTX\n9l15b4/5AT9uOnsmqHX9p5P843z+4HZGlzoF9IDyMU6+ttkR40K5udzQqtQeb6Tq\noYy7Xnf3swKBgQCCab5/Qi0sl0dYsb5jxGwKD3Dw7udPyUmdLGpq2dgM1SDin5+d\nPq4tute6f8jdAbSk7BRiMWFmoFuuJKrP3s9jDdDN1qXIOws86lyZWzybwwtz3AhR\nZsKfZdSlg2ne1pF/BqxXSIIXB2oJ+LutUwnR5MZHwb/B+tXrV1X9tDQxxQKBgCBo\n9dkQuKbKEmKUpSvWzDZqjH8SKjKYHZZDuKAVR4nVeCKV4NE5pj3XZj6tDLU7QWYB\nqTtPs5mQ1f6JrTT9OU4aeoxFzK0ES/49NFDAJ3GK9hvvhT3S7LIi0U0tb1KornFQ\nirrZpTDO699JAHB6tK/Jtc7QLTSEqmPuaM4mL/KfAoGBAKFMUu/c23wEKR1rZ3IN\nsQ/YhvZIIGjnB4gGsAlyPK9+imbbDufZQL2QzroUEG3HyZTgdJ+xmx0v08jQhfS9\nHpk12TSqiTijpXm7QkBXb8I2Lz2DsFcS0/ObOYinxwoPkvYZErYbKHYqq9Zvu62w\nTRoTE+bQdS/V45ZMWUh/S2pb\n-----END PRIVATE KEY-----\n",
-  "client_email": "jsmith-creds@rd-spreadsheet-test.iam.gserviceaccount.com",
-  "client_id": "106138092146303165019",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/jsmith-creds%40rd-spreadsheet-test.iam.gserviceaccount.com"
+  "type": st.secrets["type"],
+  "project_id": st.secrets["project_id"],
+  "private_key_id": st.secrets["private_key_id"],
+  "private_key" : st.secrets["private_key"],
+  "client_email": st.secrets["client_email"],
+  "client_id": st.secrets["client_id"],
+  "auth_uri": st.secrets["auth_uri"],
+  "token_uri": st.secrets["token_uri"],
+  "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+  "client_x509_cert_url": st.secrets["client_x509_cert_url"]
 }
 
 def update_spreadsheet(file_name, content):
