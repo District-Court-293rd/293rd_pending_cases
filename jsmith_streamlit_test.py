7e9d7808181bed2e4cb718f2a18ad97624c295b8
Johnathon Smith <smith.johnathon01@gmail.com>
2022-03-09 21:45:26 -0600

    Troubleshooting plotly issue

--- jsmith_streamlit_test.py
+++ jsmith_streamlit_test.py
@@ -11,19 +11,6 @@ import pending_upload
 import gspread
 import plotly.express as px
 
-credentials = {
-  "type": st.secrets["type"],
-  "project_id": st.secrets["project_id"],
-  "private_key_id": st.secrets["private_key_id"],
-  "private_key" : st.secrets["private_key"],
-  "client_email": st.secrets["client_email"],
-  "client_id": st.secrets["client_id"],
-  "auth_uri": st.secrets["auth_uri"],
-  "token_uri": st.secrets["token_uri"],
-  "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
-  "client_x509_cert_url": st.secrets["client_x509_cert_url"]
-}
-
 def get_file_content(file_name):
     """
     This function takes in the file data returned by the streamlit file uploader. It will
