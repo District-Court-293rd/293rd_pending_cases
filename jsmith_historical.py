85ef3a4c3d50a83031d572099dde96ad4cdfdaa7
Johnathon Smith <smith.johnathon01@gmail.com>
2022-09-17 15:50:09 -0400

    Troubleshooting. Removed info message that contained error

--- jsmith_historical.py
+++ jsmith_historical.py
@@ -152,7 +152,6 @@ def prepare_closed_cases(closed_cases):
     temp_dict['total_newly_closed_zavala_cases'] = len(closed_cases[closed_cases['County'] == 'Zavala'])
 
     #For testing, print the closed_cases dataframe and the length
-    st.info(closed_cases[0])
     st.info(closed_cases['Case Type'].iloc[0])
 
     #Get counts of case type per day and per county
