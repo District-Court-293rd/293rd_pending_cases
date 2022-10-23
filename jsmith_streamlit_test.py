2e21d1f1a2f989dc3a295c538f1211f166b61966
Johnathon Smith <smith.johnathon01@gmail.com>
2022-10-23 14:27:37 -0400

    Testing a feature that clears uploaded files after processing.

--- jsmith_streamlit_test.py
+++ jsmith_streamlit_test.py
@@ -93,71 +93,73 @@ with page_content.container():
     #Title
     st.title("Update Pending Reports")
 
-    #Get the file object
-    file_objects = st.file_uploader("File Names Must Include Either 'CR' or 'CV' for Criminal and Civil Cases, Respectively.", type = 'pdf', accept_multiple_files = True)
+    with st.form("my-form", clear_on_submit = True):
+        file_objects = st.file_uploader("File Names Must Include Either 'CR' or 'CV' for Criminal and Civil Cases, Respectively.", type = 'pdf', accept_multiple_files = True)
+        submitted = st.form_submit_button("Upload")
 
-    #Only create and update progress bar if files have been uploaded
-    if len(file_objects) > 0:
-        #Create a container for the progress bar message
-        progress_message_container = st.empty()
-        #Create a progress bar for visual verification that something is happening
-        progress_message_container.header("Please Wait, Processing In Progress:")
-        progress_bar = st.progress(0)
+    if submitted and file_objects is not None:
+        st.write("Files Uploaded!")
+        # do stuff with your uploaded file
 
-        #Use the remainder of 100 / the number of uploaded files to start the progress bar
-        bar_value = int(100 % len(file_objects))
+        #Only create and update progress bar if files have been uploaded
+        if len(file_objects) > 0:
+            #Create a container for the progress bar message
+            progress_message_container = st.empty()
+            #Create a progress bar for visual verification that something is happening
+            progress_message_container.header("Please Wait, Processing In Progress:")
+            progress_bar = st.progress(0)
 
-        #Update progress bar
-        progress_bar.progress(bar_value)
+            #Use the remainder of 100 / the number of uploaded files to start the progress bar
+            bar_value = int(100 % len(file_objects))
 
-        #How much progress should be made per file? Will need to show progress when updating historical data as well
-        progress_per_file = int((100 - bar_value) / len(file_objects))
-
-        #Also create a container for the info messages
-        info_container = st.empty()
-
-        #Use a for loop to iterate through the uploaded files
-        for file_object in file_objects:
-
-            if file_object is not None:
-
-                #Inform the user which file is being processed
-                info_container.info("Began Processing " + file_object.name)
-
-                #Save the file. Will be deleted after data is uploaded
-                with open(file_object.name, 'wb') as f:
-                    f.write(file_object.getbuffer())
-
-                #Create content container
-                content = get_file_content(file_object.name)
-
-                #Build and prepare the dataframe, then update the spreadsheet
-                pending_upload.update_spreadsheet(file_object.name, content)
-
-                #Delete the saved file. Leave messages for success or failure
-                if os.path.exists(file_object.name):
-                    os.remove(file_object.name)
-
-                    #Leave a success message
-                    st.success("Pending Reports was successfully updated with " + file_object.name)
-                
-                else:
-                    st.error("Could Not Delete File " + file_object.name)
-
-                #Clear container
-                info_container.empty()
-
-            else:
-                #Print an error message
-                st.error("A File Was Not Uploaded Correctly. Please Try Again")
-
-            #Update progress bar regardless of whether or not Pending Reports was successfully updated with the current file
-            #Add a files_uploaded flag for use in the next section
-            bar_value += progress_per_file
+            #Update progress bar
             progress_bar.progress(bar_value)
 
-        #Update message
-        progress_message_container.header("Complete! All Files Processed Successfully!")
+            #How much progress should be made per file? Will need to show progress when updating historical data as well
+            progress_per_file = int((100 - bar_value) / len(file_objects))
 
-        #Clear list of uploaded files
-        file_objects.clear()
+            #Also create a container for the info messages
+            info_container = st.empty()
+
+            #Use a for loop to iterate through the uploaded files
+            for file_object in file_objects:
+
+                if file_object is not None:
+
+                    #Inform the user which file is being processed
+                    info_container.info("Began Processing " + file_object.name)
+
+                    #Save the file. Will be deleted after data is uploaded
+                    with open(file_object.name, 'wb') as f:
+                        f.write(file_object.getbuffer())
+
+                    #Create content container
+                    content = get_file_content(file_object.name)
+
+                    #Build and prepare the dataframe, then update the spreadsheet
+                    pending_upload.update_spreadsheet(file_object.name, content)
+
+                    #Delete the saved file. Leave messages for success or failure
+                    if os.path.exists(file_object.name):
+                        os.remove(file_object.name)
+
+                        #Leave a success message
+                        st.success("Pending Reports was successfully updated with " + file_object.name)
+                
+                    else:
+                        st.error("Could Not Delete File " + file_object.name)
+
+                    #Clear container
+                    info_container.empty()
+
+                else:
+                    #Print an error message
+                    st.error("A File Was Not Uploaded Correctly. Please Try Again")
+
+                #Update progress bar regardless of whether or not Pending Reports was successfully updated with the current file
+                #Add a files_uploaded flag for use in the next section
+                bar_value += progress_per_file
+                progress_bar.progress(bar_value)
+
+            #Update message
+            progress_message_container.header("Complete! All Files Processed Successfully!")
