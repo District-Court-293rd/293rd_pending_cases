10fcf3a146a5579c87b7f12a19c4e750f4b69ed9
Johnathon Smith <smith.johnathon01@gmail.com>
2023-03-25 14:45:25 -0400

    Adding basic counts per client request - testing

--- jsmith_streamlit_test.py
+++ jsmith_streamlit_test.py
@@ -82,81 +82,95 @@ st.set_page_config(
      page_title="Pending Reports",
  )
 
-#Create a placeholder for the page content
-page_content = st.empty()
+########################################## Create Tabs ###################################################
 
-######################################### Update Pending Reports ##########################################
+tab1, tab2 = st.tabs(["Upload Reports", "View Data"])
 
-#Create container for update page content
-with page_content.container():
+######################################### Upload Reports ##########################################
+with tab1:
+    #Create a placeholder for the page content
+    page_content = st.empty()
 
-    #Title
-    st.title("Update Pending Reports")
+    #Create container for update page content
+    with page_content.container():
 
-    with st.form("my-form", clear_on_submit = True):
-        file_objects = st.file_uploader("File Names Must Include Either 'CV' or 'CR' for Civil and Criminal Cases, Respectively.", type = 'pdf', accept_multiple_files = True)
-        submitted = st.form_submit_button("Upload")
+        #Title
+        st.title("Update Pending Reports")
 
-    if submitted and len(file_objects) > 0:
-        #Give message for successful upload and begin processing
-        st.write("Files Uploaded!")
+        with st.form("my-form", clear_on_submit = True):
+            file_objects = st.file_uploader("File Names Must Include Either 'CV' or 'CR' for Civil and Criminal Cases, Respectively.", type = 'pdf', accept_multiple_files = True)
+            submitted = st.form_submit_button("Upload")
 
-        #Create a container for the progress bar message
-        progress_message_container = st.empty()
-        #Create a progress bar for visual verification that something is happening
-        progress_message_container.header("Please Wait, Processing In Progress:")
-        progress_bar = st.progress(0)
+        if submitted and len(file_objects) > 0:
+            #Give message for successful upload and begin processing
+            st.write("Files Uploaded!")
 
-        #Use the remainder of 100 / the number of uploaded files to start the progress bar
-        bar_value = int(100 % len(file_objects))
+            #Create a container for the progress bar message
+            progress_message_container = st.empty()
+            #Create a progress bar for visual verification that something is happening
+            progress_message_container.header("Please Wait, Processing In Progress:")
+            progress_bar = st.progress(0)
 
-        #Update progress bar
-        progress_bar.progress(bar_value)
+            #Use the remainder of 100 / the number of uploaded files to start the progress bar
+            bar_value = int(100 % len(file_objects))
 
-        #How much progress should be made per file?
-        progress_per_file = int((100 - bar_value) / len(file_objects))
-
-        #Also create a container for the info messages
-        info_container = st.empty()
-
-        #Use a for loop to iterate through the uploaded files
-        for file_object in file_objects:
-
-            if file_object is not None:
-
-                #Inform the user which file is being processed
-                info_container.info("Began Processing " + file_object.name)
-
-                #Save the file. Will be deleted after data is uploaded
-                with open(file_object.name, 'wb') as f:
-                    f.write(file_object.getbuffer())
-
-                #Create content container
-                content = get_file_content(file_object.name)
-
-                #Build and prepare the dataframe, then update the spreadsheet
-                pending_upload.update_spreadsheet(file_object.name, content)
-
-                #Delete the saved file. Leave messages for success or failure
-                if os.path.exists(file_object.name):
-                    os.remove(file_object.name)
-
-                    #Leave a success message
-                    st.success("Pending Reports was successfully updated with " + file_object.name)
-                
-                else:
-                    st.error("Could Not Delete File " + file_object.name)
-
-                #Clear container
-                info_container.empty()
-
-            else:
-                #Print an error message
-                st.error("A File Was Not Uploaded Correctly. Please Try Again")
-
-            #Update progress bar regardless of whether or not Pending Reports was successfully updated with the current file
-            bar_value += progress_per_file
+            #Update progress bar
             progress_bar.progress(bar_value)
 
-        #Update message
-        progress_message_container.header("Complete! All Files Processed Successfully!")
+            #How much progress should be made per file?
+            progress_per_file = int((100 - bar_value) / len(file_objects))
+
+            #Also create a container for the info messages
+            info_container = st.empty()
+
+            #Use a for loop to iterate through the uploaded files
+            for file_object in file_objects:
+
+                if file_object is not None:
+
+                    #Inform the user which file is being processed
+                    info_container.info("Began Processing " + file_object.name)
+
+                    #Save the file. Will be deleted after data is uploaded
+                    with open(file_object.name, 'wb') as f:
+                        f.write(file_object.getbuffer())
+
+                    #Create content container
+                    content = get_file_content(file_object.name)
+
+                    #Build and prepare the dataframe, then update the spreadsheet
+                    pending_upload.update_spreadsheet(file_object.name, content)
+
+                    #Delete the saved file. Leave messages for success or failure
+                    if os.path.exists(file_object.name):
+                        os.remove(file_object.name)
+
+                        #Leave a success message
+                        st.success("Pending Reports was successfully updated with " + file_object.name)
+                    
+                    else:
+                        st.error("Could Not Delete File " + file_object.name)
+
+                    #Clear container
+                    info_container.empty()
+
+                else:
+                    #Print an error message
+                    st.error("A File Was Not Uploaded Correctly. Please Try Again")
+
+                #Update progress bar regardless of whether or not Pending Reports was successfully updated with the current file
+                bar_value += progress_per_file
+                progress_bar.progress(bar_value)
+
+            #Update message
+            progress_message_container.header("Complete! All Files Processed Successfully!")
+
+with tab2:
+    st.header("View Data")
+
+    #Display a df for testing
+    df = get_spreadsheet_data("Civil Cases", credentials)
+    st.dataframe(df)
+
+    #Get count of cases without future docket dates, regardless of docket type
+    st.metric(label = "Civil Cases Without Future Docket Dates", value = df["Docket Date"].isna().sum())
