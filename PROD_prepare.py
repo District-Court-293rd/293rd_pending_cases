71ad2225bf7b0a553b78d016a0435e5086f1e08c
Johnathon Smith <smith.johnathon01@gmail.com>
2023-08-09 10:34:07 -0400

    Added logic for 'Load DateTime' column and set it to 'America/Chicago' timezone

--- PROD_prepare.py
+++ PROD_prepare.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import re
-from datetime import date
+from datetime import date, datetime
+import pytz
 
 def get_case_type(value):
     """
@@ -182,9 +183,9 @@ def prepare_dataframe(file_name, df):
     #Create Status column. Defaults to Open
     df['Status'] = 'Open'
 
-    #Removed on 05/30/2023
-    #Create Load Date column
-    #df['load_date'] = str(date.today())
+    #Create Load DateTime column
+    america_central_tz = pytz.timezone('America/Chicago')
+    df['Load DateTime'] = str(datetime.now(tz = america_central_tz))
 
     #As of 13 June 2023, we are no longer collecting names
     #Convert the lists of names in the civil cases dataframe to single strings with each name separated by a new line
